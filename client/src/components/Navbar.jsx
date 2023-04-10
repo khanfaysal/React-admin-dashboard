5ddb4a3e40de2746df9f60abb8e830dd232ce9fc
khan faysal <mdfaysalkhancse@gmail.com>
2023-04-10 13:53:01 +0600

    dark mode added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import {
     SettingsOutlined,
     ArrowDropDownOutlined,
 } from "@mui/icons-material";
+import { setMode } from 'state';
 import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
 import { useDispatch } from 'react-redux'
 import FlexBetween from './FlexBetween';
@@ -22,6 +23,7 @@ const Navbar = () => {
         }}
     >
         <Toolbar sx={{ justifyContent: 'space-between' }}>
+            {/* left side */}
             <FlexBetween>
                 <IconButton onClick={() => console.log('open sidebar')}>
                     <MenuIcon />
@@ -32,11 +34,24 @@ const Navbar = () => {
                     gap='3rem'
                     p='0.1rem 1.5rem'
                 >
-                    <InputBase placeholder='serach..' />
+                    <InputBase placeholder='search..' />
                     <IconButton>
                         <Search />
                     </IconButton>
                 </FlexBetween>
+            </FlexBetween>
+            {/* right side */}
+            <FlexBetween gap='1.5rem'>
+                <IconButton onClick={() => dispatch(setMode())}>
+                    {theme.palette.mode === 'dark' ? (
+                        <DarkModeOutlined sx={{ fontSize: '25px' }} />
+                    ) : (
+                        <LightModeOutlined sx={{ fontSize: '25px' }} />
+                    )}
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlined sx={{ fontSize: '25px' }} />
+                </IconButton>
             </FlexBetween>
         </Toolbar>
 
